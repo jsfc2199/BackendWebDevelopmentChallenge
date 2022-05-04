9973d07d989b3cb3cacebefeb8f2151650f43aff
Juan Sebastian <jsfc2199@gmail.com>
2022-05-04 09:10:12 -0500

    Springboot - Start of the backend. Sofka web development challenge

---
+++ src/main/java/com/example/sofkawebdevelopmentchallenge/SofkaWebDevelopmentChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.example.sofkawebdevelopmentchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SofkaWebDevelopmentChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SofkaWebDevelopmentChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sofkawebdevelopmentchallenge/SofkaWebDevelopmentChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sofkawebdevelopmentchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SofkaWebDevelopmentChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
